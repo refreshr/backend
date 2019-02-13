a38a795ec8af171c53a27c35118e9bd3b7d3b006
Chaya Otikor <c.otikor@gmail.com>
2019-02-13 12:12:51 -0700

    Set up postgres to connect to heroku

---
+++ server/knexfile.js
@@ -0,0 +1,49 @@
+// Update with your config settings.
+require('dotenv').config();
+const pg = require('pg');
+pg.defaults.ssl = true;
+
+module.exports = {
+	development: {
+		client: 'pg',
+		connection: process.env.DATABASE_URL,
+		migrations: {
+			directory: './data/migrations'
+		},
+		seeds: {
+			directory: './data/seeds'
+		}
+	},
+
+	staging: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	},
+
+	production: {
+		client: 'postgresql',
+		connection: {
+			database: 'my_db',
+			user: 'username',
+			password: 'password'
+		},
+		pool: {
+			min: 2,
+			max: 10
+		},
+		migrations: {
+			tableName: 'knex_migrations'
+		}
+	}
+};
